95a390d9686c14a060a70bcd1df10c15efaee570
Prabh Singh <prabhm512@gmail.com>
2021-03-01 18:59:16 +1100

    hero list created & only details of selected hero are rendered

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroInterface';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,16 @@ import { Hero } from '../heroInterface';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-
 }
