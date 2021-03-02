4a82270420a599c83736c7b0784fa7fd96ccbed6
Prabh Singh <prabhm512@gmail.com>
2021-03-02 12:07:50 +1100

    add new hero feature created

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,4 +26,14 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
+
+  // Add a new hero
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 }
